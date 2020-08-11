519798bcc0f8c3ec08b69f28ba635409465cf6d6
Roman <first.luker@gmail.com>
2020-08-11 20:58:52 +0300

    Added models.

---
+++ SocNetw/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SocNetw
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public byte[] Image { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+        public string Email {get; set; }
+        public string PhoneNubmer { get; set; }
+        public string TitleDescrioption { get; set; }
+        public string Descrioption { get; set; }
+    }
+}
